73d8332fdbe5d13ba57957191e529a7b5aea66da
SMSLink.ro <contact@smslink.ro>
2020-08-15 20:16:13 +0300

    Create main.vb

---
+++ main.vb
@@ -0,0 +1,15 @@
+Imports System
+				
+Public Module VBModule
+	Public Sub Main()
+        Dim webClient As New System.Net.WebClient
+        
+        '
+        '   Get your SMSLink / SMS Gateway Connection ID and Password from 
+        '     https://www.smslink.ro/get-api-key/
+        '
+        
+        Dim result As String = webClient.DownloadString("https://secure.smslink.ro/sms/gateway/communicate/index.php?connection_id=... My Connection ID ...&password=... My Connection Password ...&to=07xyzzzzzz&message=test")
+        Console.WriteLine(result)
+    End Sub
+End Module
