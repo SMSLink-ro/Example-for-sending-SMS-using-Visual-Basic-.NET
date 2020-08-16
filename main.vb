01d1778a9980caac288ae17372907d897a030861
SMSLink.ro <contact@smslink.ro>
2020-08-16 19:07:12 +0300

    Update main.vb

--- main.vb
+++ main.vb
@@ -9,6 +9,9 @@ Public Module VBModule
         '     https://www.smslink.ro/get-api-key/
         '
         
+        '		HTTPS API Endpoint:  https://secure.smslink.ro/sms/gateway/communicate/index.php
+	'		HTTP API Endpoint:   http://www.smslink.ro/sms/gateway/communicate/index.php
+
         Dim result As String = webClient.DownloadString("https://secure.smslink.ro/sms/gateway/communicate/index.php?connection_id=... My Connection ID ...&password=... My Connection Password ...&to=07xyzzzzzz&message=test")
         Console.WriteLine(result)
     End Sub
